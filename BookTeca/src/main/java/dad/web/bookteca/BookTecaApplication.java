399781172ed689b8e3a9b23901829d7ff5bb3597
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2020-01-23 11:13:10 +0100

    Crear proyecto

---
+++ BookTeca/src/main/java/dad/web/bookteca/BookTecaApplication.java
@@ -0,0 +1,13 @@
+package dad.web.bookteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTecaApplication.class, args);
+	}
+
+}
---
+++ BookTeca/src/test/java/dad/web/bookteca/BookTecaApplicationTests.java
@@ -0,0 +1,13 @@
+package dad.web.bookteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
