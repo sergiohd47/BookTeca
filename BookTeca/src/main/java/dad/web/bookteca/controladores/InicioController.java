2d97f526181eb57482f98d9ca234fade65658612
BorjaPiruXx <bormaral13@gmail.com>
2020-02-14 20:12:17 +0100

    put methods in the corresponding controller
    Nuevo controlador de usuario que hara las diferentes tareas de poder buscar libros/revistas/salas de trabajo/equipos informaticos libres para asi reservarlas posteriormente.

---
+++ BookTeca/src/main/java/dad/web/bookteca/controladores/InicioController.java
@@ -0,0 +1,128 @@
+package dad.web.bookteca.controladores;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import dad.web.bookteca.clases.EquipoInformatico;
+import dad.web.bookteca.clases.Libro;
+import dad.web.bookteca.clases.Revista;
+import dad.web.bookteca.clases.SalaTrabajoGrupo;
+import dad.web.bookteca.clases.Usuario;
+import dad.web.bookteca.basedatos.*;
+
+@Controller
+public class InicioController {
+
+	protected final int NUMERO_RECURSOS_MAIN=3; //NUMERO DE LIBROS Y REVISTAS QUE SE MUESTRAN EN LA PANTALLA DE INICIO
+
+	@Autowired
+	private EquipoInformaticoRepository equiposInformaticos;
+
+	@Autowired
+	private LibroRepository libros;
+
+	@Autowired
+	private RevistaRepository revistas;
+
+	@Autowired
+	private SalaTrabajoGrupoRepository salasTrabajoGrupo;
+
+	@Autowired
+	private UsuarioRepository usuarios;
+
+	public static ArrayList<Libro> listaLibrosDestacados;
+	public static ArrayList<Revista> listaRevistasDestacadas;
+
+	@PostConstruct
+	public void init() {
+		
+		/*//LIBROS
+		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
+		libros.save(new Libro("Los Tres Cerditos", "Borja","El Mundo","Infantil"));
+		libros.save(new Libro("La Vuelta Al Mundo En 80 Dias", "Dani","Santillana","Viajes"));
+		libros.save(new Libro("Constitucion Española","Indef","España","Institucional"));
+		libros.save(new Libro("Derecho Mercantil Español","Cristina","AAA","Educativo"));
+		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
+		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
+		libros.save(new Libro("La Vuelta Al Mundo En 80 Dias", "Dani","Santillana","Viajes"));
+
+		//REVISTAS
+		revistas.save(new Revista("GQ", "Editorial GQ",123,"Actualidad"));
+		revistas.save(new Revista("FHM", "Editorial FHM",35,"Erotica"));
+		revistas.save(new Revista("MasQueCoches", "Editorial MasQueCoches",77,"Automovil"));
+		revistas.save(new Revista("AutoSport", "Editorial AutoSport",23,"Automovil"));
+		revistas.save(new Revista("MuyInteresante", "Editorial MuyInteresante",456,"Intelectual"));
+
+		//SALAS
+		salasTrabajoGrupo.save(new SalaTrabajoGrupo(16,"Planta 1",true));
+		salasTrabajoGrupo.save(new SalaTrabajoGrupo(3,"Planta 0",false));
+		salasTrabajoGrupo.save(new SalaTrabajoGrupo(10,"Planta 2",true));
+		salasTrabajoGrupo.save(new SalaTrabajoGrupo(5,"Planta 0",false));
+
+		//EQUIPO INFORMATICO
+		equiposInformaticos.save(new EquipoInformatico("MacOSX","Equipo 45"));
+		equiposInformaticos.save(new EquipoInformatico("Linux","Equipo 32"));
+		equiposInformaticos.save(new EquipoInformatico("Windows","Equipo 2"));
+		equiposInformaticos.save(new EquipoInformatico("MacOSX","Equipo 1"));
+
+		//USUARIOS
+		usuarios.save(new Usuario("Borja","Martin Alonso","G07martin","bormaral13@gmail.com",false));
+		usuarios.save(new Usuario("Sergio","Hernandez Dominguez","Pass1","sergiohd47@gmail.com",true));
+		usuarios.save(new Usuario("Daniel","Molina Ballesteros","Daany10","dmolinaballesteros@gmail.com",false));*/
+		
+	}
+
+	@RequestMapping("/")
+	public String inicio(Model model) {
+		listaLibrosDestacados=new ArrayList<>();
+		listaRevistasDestacadas=new ArrayList<>();
+		ArrayList<Libro> listaLibros=(ArrayList<Libro>) libros.findAll();
+		Collections.shuffle(listaLibros);
+		int i = 0;
+		int j = 0;
+		ArrayList<String> nombresLibros = new ArrayList<>(NUMERO_RECURSOS_MAIN);
+		do {
+			if(!nombresLibros.contains(listaLibros.get(j).getNombre()) && (listaLibros.get(j).isDisponible())) {
+				nombresLibros.add(listaLibros.get(j).getNombre());
+				listaLibrosDestacados.add(listaLibros.get(j));
+				j++;
+				i++;
+			} else
+				j++;
+		} while(i < NUMERO_RECURSOS_MAIN);
+		model.addAttribute("listaLibrosDestacados",listaLibrosDestacados);
+		ArrayList<Revista> listaRevistas=(ArrayList<Revista>) revistas.findAll();
+		Collections.shuffle(listaRevistas);
+		ArrayList<String> nombresRevistas = new ArrayList<>(NUMERO_RECURSOS_MAIN);
+		i = 0;
+		j = 0;
+		do {
+			if(!nombresRevistas.contains(listaRevistas.get(j).getNombre()) && (listaRevistas.get(j).isDisponible())) {
+				nombresRevistas.add(listaRevistas.get(j).getNombre());
+				listaRevistasDestacadas.add(listaRevistas.get(j));
+				j++;
+				i++;
+			} else
+				j++;
+		} while(i < NUMERO_RECURSOS_MAIN);
+		model.addAttribute("listaRevistasDestacadas",listaRevistasDestacadas);
+
+		//NO HA INICIADO SESION
+		model.addAttribute("visibleIniciarSesion",true);
+		return "index";
+	}
+
+}
--- BookTeca/src/main/java/dad/web/bookteca/controladores/UsuarioController.java
+++ BookTeca/src/main/java/dad/web/bookteca/controladores/UsuarioController.java
@@ -1,32 +1,28 @@
 package dad.web.bookteca.controladores;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
+import dad.web.bookteca.basedatos.EquipoInformaticoRepository;
+import dad.web.bookteca.basedatos.LibroRepository;
+import dad.web.bookteca.basedatos.RevistaRepository;
+import dad.web.bookteca.basedatos.SalaTrabajoGrupoRepository;
+import dad.web.bookteca.basedatos.UsuarioRepository;
 import dad.web.bookteca.clases.EquipoInformatico;
 import dad.web.bookteca.clases.Libro;
 import dad.web.bookteca.clases.Revista;
 import dad.web.bookteca.clases.SalaTrabajoGrupo;
 import dad.web.bookteca.clases.Usuario;
-import dad.web.bookteca.basedatos.*;
 
 @Controller
-public class inicioController {
-
-	protected final int NUMERO_RECURSOS_MAIN=3; //NUMERO DE LIBROS Y REVISTAS QUE SE MUESTRAN EN LA PANTALLA DE INICIO
+public class UsuarioController {
 
 	@Autowired
 	private EquipoInformaticoRepository equiposInformaticos;
@@ -42,120 +38,9 @@ public class inicioController {
 
 	@Autowired
 	private UsuarioRepository usuarios;
-
-	private ArrayList<Libro> listaLibrosDestacados;
-	private ArrayList<Revista> listaRevistasDestacadas;
+	
 	private boolean sesionNoIniciada = true;
-
-	@PostConstruct
-	public void init() {
-		
-		/*//LIBROS
-		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
-		libros.save(new Libro("Los Tres Cerditos", "Borja","El Mundo","Infantil"));
-		libros.save(new Libro("La Vuelta Al Mundo En 80 Dias", "Dani","Santillana","Viajes"));
-		libros.save(new Libro("Constitucion Española","Indef","España","Institucional"));
-		libros.save(new Libro("Derecho Mercantil Español","Cristina","AAA","Educativo"));
-		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
-		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
-		libros.save(new Libro("La Vuelta Al Mundo En 80 Dias", "Dani","Santillana","Viajes"));
-
-		//REVISTAS
-		revistas.save(new Revista("GQ", "Editorial GQ",123,"Actualidad"));
-		revistas.save(new Revista("FHM", "Editorial FHM",35,"Erotica"));
-		revistas.save(new Revista("MasQueCoches", "Editorial MasQueCoches",77,"Automovil"));
-		revistas.save(new Revista("AutoSport", "Editorial AutoSport",23,"Automovil"));
-		revistas.save(new Revista("MuyInteresante", "Editorial MuyInteresante",456,"Intelectual"));
-
-		//SALAS
-		salasTrabajoGrupo.save(new SalaTrabajoGrupo(16,"Planta 1",true));
-		salasTrabajoGrupo.save(new SalaTrabajoGrupo(3,"Planta 0",false));
-		salasTrabajoGrupo.save(new SalaTrabajoGrupo(10,"Planta 2",true));
-		salasTrabajoGrupo.save(new SalaTrabajoGrupo(5,"Planta 0",false));
-
-		//EQUIPO INFORMATICO
-		equiposInformaticos.save(new EquipoInformatico("MacOSX","Equipo 45"));
-		equiposInformaticos.save(new EquipoInformatico("Linux","Equipo 32"));
-		equiposInformaticos.save(new EquipoInformatico("Windows","Equipo 2"));
-		equiposInformaticos.save(new EquipoInformatico("MacOSX","Equipo 1"));
-
-		//USUARIOS
-		usuarios.save(new Usuario("Borja","Martin Alonso","G07martin","bormaral13@gmail.com",false));
-		usuarios.save(new Usuario("Sergio","Hernandez Dominguez","Pass1","sergiohd47@gmail.com",true));
-		usuarios.save(new Usuario("Daniel","Molina Ballesteros","Daany10","dmolinaballesteros@gmail.com",false));*/
-		
-	}
-
-	@RequestMapping("/")
-	public String inicio(Model model) {
-		listaLibrosDestacados=new ArrayList<>();
-		listaRevistasDestacadas=new ArrayList<>();
-		ArrayList<Libro> listaLibros=(ArrayList<Libro>) libros.findAll();
-		Collections.shuffle(listaLibros);
-		int i = 0;
-		int j = 0;
-		ArrayList<String> nombresLibros = new ArrayList<>(NUMERO_RECURSOS_MAIN);
-		do {
-			if(!nombresLibros.contains(listaLibros.get(j).getNombre())) {
-				nombresLibros.add(listaLibros.get(j).getNombre());
-				listaLibrosDestacados.add(listaLibros.get(j));
-				j++;
-				i++;
-			} else
-				j++;
-		} while(i < NUMERO_RECURSOS_MAIN);
-		model.addAttribute("listaLibrosDestacados",listaLibrosDestacados);
-		ArrayList<Revista> listaRevistas=(ArrayList<Revista>) revistas.findAll();
-		Collections.shuffle(listaRevistas);
-		ArrayList<String> nombresRevistas = new ArrayList<>(NUMERO_RECURSOS_MAIN);
-		i = 0;
-		j = 0;
-		do {
-			if(!nombresRevistas.contains(listaRevistas.get(j).getNombre())) {
-				nombresRevistas.add(listaRevistas.get(j).getNombre());
-				listaRevistasDestacadas.add(listaRevistas.get(j));
-				j++;
-				i++;
-			} else
-				j++;
-		} while(i < NUMERO_RECURSOS_MAIN);
-		model.addAttribute("listaRevistasDestacadas",listaRevistasDestacadas);
-
-		//NO HA INICIADO SESION
-		model.addAttribute("visibleIniciarSesion",true);
-		return "index";
-	}
-
-	@RequestMapping("/iniciarSesion")
-	public String iniciarSesion(Model model) {
-		return "iniciarSesionNuevo";
-	}
-
-	@RequestMapping("/sesionIniciada")
-	public String sesionIniciada(Model model, @RequestParam("nombreUsuario") String email,@RequestParam String contrasenya, HttpSession usuarioSesion) {
-		Usuario usuario=usuarios.findByEmailAndContrasenya(email,contrasenya);
-		if(usuario==null)
-			return iniciarSesion(model);
-		usuarioSesion.setAttribute("infoUsuario",usuario);
-		sesionNoIniciada = false;
-		if(!usuario.getAdministrador()) {
-			model.addAttribute("usuario",true);
-			model.addAttribute("listaLibrosDestacados",listaLibrosDestacados);
-			model.addAttribute("listaRevistasDestacadas",listaRevistasDestacadas);
-		} else {
-			model.addAttribute("usuarioAdmin",true);
-			ArrayList<Libro> listaLibros=(ArrayList<Libro>) libros.findAll();
-			model.addAttribute("listaLibros",listaLibros);
-			ArrayList<Revista> listaRevistas=(ArrayList<Revista>) revistas.findAll();
-			model.addAttribute("listaRevistas",listaRevistas);
-			ArrayList<SalaTrabajoGrupo> listaSTGs=(ArrayList<SalaTrabajoGrupo>) salasTrabajoGrupo.findAll();
-			model.addAttribute("listaSTGs",listaSTGs);
-			ArrayList<EquipoInformatico> listaEquipos=(ArrayList<EquipoInformatico>) equiposInformaticos.findAll();
-			model.addAttribute("listaEquipos",listaEquipos);
-		}
-		return "sesionIniciada";
-	}
-
+	
 	@RequestMapping("/buscadorLibros")
 	public String buscadorLibros(Model model, HttpSession usuarioSesion) {
 		Usuario usuario=(Usuario) usuarioSesion.getAttribute("infoUsuario");
@@ -172,16 +57,21 @@ public class inicioController {
 	@RequestMapping("/busquedaLibros")
 	public String busquedaLibros(Model model, HttpSession usuarioSesion, @RequestParam("palabraClaveLibro") String info) {
 		Usuario usuario=(Usuario) usuarioSesion.getAttribute("infoUsuario");
-		ArrayList<Libro> listaLibrosBusqueda=new ArrayList<>();
-		listaLibrosBusqueda.addAll(libros.findByNombreOrAutorOrEditorialOrGenero(info,info,info,info));
+		ArrayList<Libro>listaLibros=new ArrayList<>();
+		listaLibros.addAll(libros.findByNombreOrAutorOrEditorialOrGenero(info,info,info,info));
 		if(sesionNoIniciada) {
 			model.addAttribute("visibleIniciarSesion",true);
-			model.addAttribute("listaLibrosBusqueda",listaLibrosBusqueda);
-			model.addAttribute("visibleTabla",!listaLibrosBusqueda.isEmpty());
+			model.addAttribute("listaLibrosBusqueda",listaLibros);
+			model.addAttribute("visibleTabla",!listaLibros.isEmpty());
 		} else {
+			ArrayList<Libro> listaLibrosBusqueda=new ArrayList<>();
 			model.addAttribute("visibleCerrarSesion",true);
 			if(!usuario.getAdministrador()) {
 				model.addAttribute("nombre",usuario.getNombre());
+				for(Libro l : listaLibros) {
+					if(l.isDisponible())
+						listaLibrosBusqueda.add(l);
+				}
 				model.addAttribute("listaLibrosBusqueda",listaLibrosBusqueda);
 				model.addAttribute("visibleTabla",!listaLibrosBusqueda.isEmpty());
 			}
@@ -211,16 +101,21 @@ public class inicioController {
 	@RequestMapping("/busquedaRevistas")
 	public String busquedaRevistas(Model model, HttpSession usuarioSesion, @RequestParam("palabraClaveRevista") String info) {
 		Usuario usuario=(Usuario) usuarioSesion.getAttribute("infoUsuario");
-		ArrayList<Revista> listaRevistasBusqueda=new ArrayList<>();
-		listaRevistasBusqueda.addAll(revistas.findByNombreOrEditorialOrGenero(info,info,info));
+		ArrayList<Revista> listaRevistas=new ArrayList<>();
+		listaRevistas.addAll(revistas.findByNombreOrEditorialOrGenero(info,info,info));
 		if(sesionNoIniciada) {
 			model.addAttribute("visibleIniciarSesion",true);
-			model.addAttribute("listaRevistasBusqueda",listaRevistasBusqueda);
-			model.addAttribute("visibleTabla",!listaRevistasBusqueda.isEmpty());
+			model.addAttribute("listaRevistasBusqueda",listaRevistas);
+			model.addAttribute("visibleTabla",!listaRevistas.isEmpty());
 		} else {
+			ArrayList<Revista> listaRevistasBusqueda=new ArrayList<>();
 			model.addAttribute("visibleCerrarSesion",true);
 			if(!usuario.getAdministrador()) {
 				model.addAttribute("nombre",usuario.getNombre());
+				for(Revista r : listaRevistas) {
+					if(r.isDisponible())
+						listaRevistasBusqueda.add(r);
+				}
 				model.addAttribute("listaRevistasBusqueda",listaRevistasBusqueda);
 				model.addAttribute("visibleTabla",!listaRevistasBusqueda.isEmpty());
 			}
@@ -238,7 +133,6 @@ public class inicioController {
 			boolean visibleTabla=!listaSTG.isEmpty();
 			model.addAttribute("visibleTabla",visibleTabla);
 			model.addAttribute("listaSTG",listaSTG);
-			//model.addAttribute("visibleIniciarSesion",!usuarios.findAll().contains(usuario));
 		} else {
 			model.addAttribute("visibleCerrarSesion",true);
 			if(!usuario.getAdministrador()) {
@@ -269,16 +163,49 @@ public class inicioController {
 		}
 		return "reservaEquipoInformatico";
 	}
+	
+	@RequestMapping("/iniciarSesion")
+	public String iniciarSesion(Model model) {
+		return "iniciarSesion";
+	}
+
+	@RequestMapping("/sesionIniciada")
+	public String sesionIniciada(Model model, @RequestParam("nombreUsuario") String email,@RequestParam String contrasenya, HttpSession usuarioSesion) {
+		Usuario usuario=usuarios.findByEmailAndContrasenya(email,contrasenya);
+		if(usuario==null)
+			return iniciarSesion(model);
+		usuarioSesion.setAttribute("infoUsuario",usuario);
+		sesionNoIniciada = false;
+		if(!usuario.getAdministrador()) {
+			model.addAttribute("usuario",true);
+			model.addAttribute("listaLibrosDestacados",InicioController.listaLibrosDestacados);
+			model.addAttribute("listaRevistasDestacadas",InicioController.listaRevistasDestacadas);
+		} else {
+			model.addAttribute("usuarioAdmin",true);
+			ArrayList<Libro> listaLibros=(ArrayList<Libro>) libros.findAll();
+			model.addAttribute("listaLibros",listaLibros);
+			ArrayList<Revista> listaRevistas=(ArrayList<Revista>) revistas.findAll();
+			model.addAttribute("listaRevistas",listaRevistas);
+			ArrayList<SalaTrabajoGrupo> listaSTGs=(ArrayList<SalaTrabajoGrupo>) salasTrabajoGrupo.findAll();
+			model.addAttribute("listaSTGs",listaSTGs);
+			ArrayList<EquipoInformatico> listaEquipos=(ArrayList<EquipoInformatico>) equiposInformaticos.findAll();
+			model.addAttribute("listaEquipos",listaEquipos);
+		}
+		return "sesionIniciada";
+	}
 
 	@RequestMapping("/registro")
 	public String registro(Model model) {
 		return "registro";
 	}
 	@RequestMapping("/iniciarSesionTrasRegistro")
-	public String iniciarSesionTrasRegistro(Model model, @RequestParam("nombreUsuario") String nombre, @RequestParam("email") String email,
-			@RequestParam("contrasenya") String contraseña, @RequestParam("apellidosUsuario") String apellidos) {
+	public String iniciarSesionTrasRegistro(Model model, @RequestParam("nombreUsuario") String nombre, 
+			@RequestParam("apellidosUsuario") String apellidos, @RequestParam String email,
+			@RequestParam("contrasenya") String contraseña) {
 		usuarios.save(new Usuario(nombre,apellidos,contraseña,email,false));
-		return "iniciarSesionNuevoRegistro";
+		model.addAttribute("email",email);
+		model.addAttribute("contraseña",contraseña);
+		return "iniciarSesionTrasRegistro";
 	}
 
 	@RequestMapping("/miPerfil")
@@ -309,9 +236,19 @@ public class inicioController {
 		}
 		return "miPerfil";
 	}
+	
+	@RequestMapping("/editarPerfil")
+	public String editarPerfil(Model model, HttpSession sesionUsuario ) {
+		Usuario usuario = (Usuario) sesionUsuario.getAttribute("infoUsuario");
+		model.addAttribute("nombre",usuario.getNombre());
+		model.addAttribute("apellidos",usuario.getApellidos());
+		model.addAttribute("contraseña",usuario.getContrasenya());
+		return "editarPerfil";
+	}
+	
 	@RequestMapping("/perfilEditado")
 	public String perfilEditado(Model model, HttpSession sesionUsuario, @RequestParam("nuevoNombreUsuario") String nombre, 
-			@RequestParam("nuevoApellidoUsuario") String apellidos, @RequestParam("nuevaContrasenya") String contrasenya) {
+			@RequestParam("nuevosApellidosUsuario") String apellidos, @RequestParam("nuevaContrasenya") String contrasenya) {
 		Usuario usuarioEditado = (Usuario) sesionUsuario.getAttribute("infoUsuario");
 		usuarioEditado.setNombre(nombre);
 		usuarioEditado.setApellidos(apellidos);
@@ -323,10 +260,4 @@ public class inicioController {
 		return "perfilEditado";
 		
 	}
-
-	@RequestMapping("/editarPerfil")
-	public String editarPerfil(Model model, HttpSession sesionUsuario ) {
-		return "editarPerfil";
-	}
-
 }
