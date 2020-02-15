2061a64cb59f3b277a03f2d943f0b80b177d25b5
danyelmb <36004911+danyelmb@users.noreply.github.com>
2020-02-15 19:34:18 +0100

    reservarLibro metodo

--- BookTeca/src/main/java/dad/web/bookteca/clases/Usuario.java
+++ BookTeca/src/main/java/dad/web/bookteca/clases/Usuario.java
@@ -1,9 +1,14 @@
 package dad.web.bookteca.clases;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.sql.Date;
+import java.time.LocalDate;
 
 import javax.persistence.*;
+
+import dad.web.bookteca.controladores.InicioController;
 
 @Entity
 public class Usuario {
@@ -138,9 +143,21 @@ public class Usuario {
 		this.puestoInformatico = puestoInformatico;
 	}
 	
-	public void reservarLibro(Libro l) {
-		if(!librosReservados.contains(l))
-			librosReservados.add(l);
+	public boolean reservarLibro(Libro l) {
+		if (l.isDisponible()){
+				if (librosReservados.size()<3) {
+					librosReservados.add(l);
+					Calendar calendar = Calendar.getInstance();
+				    
+					Date inicio = Date.valueOf(java.time.LocalDate.now());
+					calendar.setTime(inicio); // Configuramos la fecha que se recibe
+				    calendar.add(Calendar.DAY_OF_YEAR , 7);
+					Date fin = (Date) calendar.getTime();
+					l.reservar(this, inicio, fin);
+					return true;
+				}
+		}
+		return false;
 	}
 	
 	public void reservarRevista(Revista r) {
