d764aba04fdd79241c91c118b271b392c532003b
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2020-02-17 17:25:47 +0100

    Añadiendo capturas (final)
    Añadidas las ultimas capturas para poder empezar con el readme

--- BookTeca/src/main/java/dad/web/bookteca/controladores/InicioController.java
+++ BookTeca/src/main/java/dad/web/bookteca/controladores/InicioController.java
@@ -48,7 +48,7 @@ public class InicioController {
 
 	@PostConstruct
 	public void init() {
-		
+		/*
 		//LIBROS
 		libros.save(new Libro("Caperucita Roja", "Sergio","Anaya","Infantil"));
 		libros.save(new Libro("Los Tres Cerditos", "Borja","El Mundo","Infantil"));
@@ -82,7 +82,7 @@ public class InicioController {
 		usuarios.save(new Usuario("Borja","Martin Alonso","G07martin","bormaral13@gmail.com",false));
 		usuarios.save(new Usuario("Sergio","Hernandez Dominguez","Pass1","sergiohd47@gmail.com",true));
 		usuarios.save(new Usuario("Daniel","Molina Ballesteros","Daany10","dmolinaballesteros@gmail.com",false));
-		
+		*/
 	}
 
 	@RequestMapping("/")
