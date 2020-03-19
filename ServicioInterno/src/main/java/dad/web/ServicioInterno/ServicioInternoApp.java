afe46245ec69a6cdb52a02fbfb5612a7f04208ca
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2020-03-19 12:07:13 +0100

    Servicio Interno funcionando

--- ServicioInterno/src/main/java/dad/web/ServicioInterno/ServicioInternoApp.java
+++ ServicioInterno/src/main/java/dad/web/ServicioInterno/ServicioInternoApp.java
@@ -2,6 +2,7 @@ package dad.web.ServicioInterno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ServicioInternoApp {
